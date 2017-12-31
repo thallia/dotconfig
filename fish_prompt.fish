4a5e0b0327a8a4aa9e694e2c29b2e06d404e8019
thallia <thalliatree@gmail.com>
2017-12-30 16:28:52 -0800

    Create fish_prompt.fish

---
+++ fish_prompt.fish
@@ -0,0 +1,42 @@
+function fish_prompt
+  echo -n " "
+  # winter
+  #set_color 198c8c
+  #echo -n ">"
+  #set_color b0e0e6
+  #echo -n ">"
+  #set_color e7f5f7
+  #echo -n ">"
+
+  # fire
+  #set_color f6412d
+  #echo -n ">"
+  #set_color ff9800
+  #echo -n ">"
+  #set_color ffc100
+  #echo -n ">"
+  #set_color ffec19
+  #echo -n ">"
+
+  # rainbow prompt
+  set_color e32636
+  echo -n ">"
+  set_color ff8d00
+  echo -n ">"
+  set_color 00dc6e
+  echo -n ">"
+  set_color 006eca
+  echo -n ">"
+
+   # shiverwind
+   # echo -n ">"
+   #set_color bf72ff
+   #echo -n ">"
+   #set_color 6db5f9
+   #echo -n ">"
+   #set_color 76f69c
+   #echo -n ">"
+
+# end
+   echo -n "|"
+                                                      1,1           Top
